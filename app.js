e25489b03f83268e6791280e2efbf7839f980f84
Payad <swordoflight8847@yahoo.com>
2023-04-01 10:57:10 -0700

    created logic for clock

---
+++ app.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000)
+
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds()/60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes())/60;
+    const hoursRatio = (minutesRatio + currentDate.getHours())/12;
+
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+const secondHand = document.querySelector('[data-hand-second]');
+const minuteHand = document.querySelector('[data-hand-minute]');
+const hourHand = document.querySelector('[data-hand-hour]');
